315f07085133957c0eec8da388724735f874c54b
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-05 21:00:38 -0500

    created the files for the schemas folder and added the basic logic to each

---
+++ server/schemas/interestOperations/addInterestsToGroup.js
@@ -0,0 +1,22 @@
+const Group = require('../models/Group');
+
+const addInterestToGroup = async (groupId, interestId) => {
+    try {
+        const group = await Group.findById(groupId);
+        if (!group) {
+            throw new Error('Group not found');
+        }
+
+        // Check if the interest is already added to the group
+        if (!group.interests.includes(interestId)) {
+            group.interests.push(interestId);
+            await group.save();
+        }
+
+        return group;
+    } catch (error) {
+        throw new Error('Error while adding interest to group: ' + error.message);
+    }
+};
+
+module.exports = addInterestToGroup;
---
+++ server/schemas/userOperations/deleteUser.js
@@ -0,0 +1,32 @@
+const User = require('../models/User');
+
+const deleteUser = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        await user.remove();
+        return { message: 'User deleted successfully' };
+    } catch (error) {
+        throw new Error('Error while deleting user: ' + error.message);
+    }
+};
+
+module.exports = deleteUser;
+
+// for a cascade delete that would delete the user and all their posts and comments. It would have to be refrenced anywhere else a user comes up
+// const User = require('../models/User');
+
+// const deleteUser = async (userId) => {
+//     try {
+//         const user = await User.findById(userId);
+//         if (!user) {
+//             throw new Error('User not found');
+//         }
+//         await user.remove(); // Important: Use remove() method on the document, not on the model
+//         return { message: 'User deleted successfully' };
+//     } catch (error) {
+//         throw new Error('Error while deleting user: ' + error.message);
+//     }
+// };
