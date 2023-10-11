90b6cfaba8c55676d0b8514f1d3c3bb0c8af4398
jakelipscomb <jlipscombtx95@gmail.com>
2023-10-10 20:36:46 -0500

    added components including a navbar, heroSection, Contact, and more. Imported themes to use for mobile first development. fixed to display in app and main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createTheme, ThemeProvider } from '@mui/material'
-import CssBaseline from '@mui/material/CssBaseline'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// import App from './App.jsx';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import Signup from './pages/Signup';
+// import Profile from './pages/Profile';
+// import Error from './pages/Error';
+
+
 
 const theme = createTheme({
   palette: {
@@ -21,14 +32,41 @@ const theme = createTheme({
       fontWeight: 'bold',
     },
   },
-
 })
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     error: <Error />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />
+//       }, {
+//         path: '/login',
+//         element: <Login />
+//       }, {
+//         path: '/signup',
+//         element: <Signup />
+//       }, {
+//         path: '/me',
+//         element: <Profile />
+//       }, {
+//         path: '/',
+//         element: <Profile />
+//       }
+//     ]
+//   }
+// ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
+      {/* <RouterProvider router={router}> */}
+        <CssBaseline />
         <App />
+      {/* </RouterProvider> */}
     </ThemeProvider>
   </React.StrictMode>,
 )
