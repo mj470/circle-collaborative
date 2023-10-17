e542c5e973029d898203bb90fa2ae674bef0585a
jakelipscomb <jlipscombtx95@gmail.com>
2023-10-16 19:41:12 -0500

    changes to pages in nav to help with rendering

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/groups',
+        path: '/circlegroups',
         element: <CircleGroupsPage />,
       },
       {
