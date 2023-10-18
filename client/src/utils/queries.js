98a9c2bd9d1450494daf0ae2e06c60001e2e19f9
cmdnguyen <cmdnguyen94@gmail.com>
2023-10-17 20:07:46 -0500

    Added Queries and Mutations in client folder

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,66 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
-  query allProfiles {
-    profiles {
-      _id
-      name
-      groups
-    }
+export const QUERY_USERS = gql`
+query users {
+  users {
+    _id
+    username
   }
+}
 `;
 
-export const QUERY_SINGLE_PROFILE = gql`
-  query singleProfile($profileId: ID!) {
-    profile(profileId: $profileId) {
-      _id
-      name
-      groups
-    }
+export const QUERY_SINGLE_USER = gql`
+query singleUser($username: String!) {
+  user(username: $username) {
+    _id
+    username
   }
+}
 `;
 
-export const QUERY_ME = gql`
-  query me {
-    me {
+export const QUERY_GROUPS = gql`
+query allGroups {
+  allGroups {
+    _id
+    groupName
+    groupDescription
+  }
+}
+`;
+
+export const QUERY_SINGLE_GROUP = gql`
+query singleGroup($groupId: ID!) {
+  group(groupId: $groupId) {
+    _id
+    groupName
+    groupDescription
+    users {
+      username
       _id
-      name
-      groups
+    }
+    posts {
+      _id
+      postText
+      postAuthor
     }
   }
-`;
+}
+`
+
+export const QUERY_SINGLE_POST = gql`
+query post($postId: ID!) {
+  post(postId: $postId) {
+    _id
+    postText
+    createdAt
+    postAuthor
+    comments {
+      _id
+      commentAuthor
+      commentText
+      createdAt
+    }
+  }
+}
+`
+
