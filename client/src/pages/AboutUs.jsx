43ec0b3646c721c29a4d93a3140659f8c9ca1aca
jakelipscomb <jlipscombtx95@gmail.com>
2023-10-19 01:24:02 -0500

    about us page

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,59 +1,70 @@
 import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
+import member1 from '../assets/images/Jake.png';
+import member2 from '../assets/images/Catherine.png';
+import member3 from '../assets/images/Maren.png';
+import member4 from '../assets/images/Rob.png';
 
-function About() {
+const teamMembers = [
+  {
+    name: 'Jake Lipscomb',
+    role: 'Developer',
+    image: member1,
+    github: 'https://github.com/jakelipscomb',
+    linkedin: 'https://www.linkedin.com/in/jake-lipscomb/'
+  },
+  {
+    name: 'Catherine Nguyen',
+    role: 'Developer',
+    image: member2,
+    github: 'https://github.com/cmdnguyen',
+    linkedin: 'https://www.linkedin.com/in/cmdnguyen/'
+  },
+  {
+    name: 'Maren Christensen',
+    role: 'Developer',
+    image: member3,
+    github: 'https://github.com/mj470',
+    linkedin: 'https://www.linkedin.com/in/maren-christensen-41339510b/'
+  },
+  {
+    name: 'Robbin Collins Jr',
+    role: 'Developer',
+    image: member4,
+    github: 'https://github.com/robbi19',
+    linkedin: 'https://www.linkedin.com/',
+  },
+];
+
+const About = () => {
   const theme = useTheme();
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6}>
-        <Card sx={{ display: 'flex', backgroundColor: theme.palette.background.paper }}>
-          <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
-          <CardContent sx={{ flex: '1 0 auto', p:5, m:1}}>
-  <Typography variant="h5" color="primary" sx={{ flex: '1 0 auto', textAlign: 'center' }}>
-    About Me
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    Hi, my name is Jake Lipscomb, and I am a full-stack web developer. Welcome to my portfolio.
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    I attended school at the University of Texas at Austin where I fell in love with the landscape and arts which landed me a job at the Blanton Museum of Art.
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    In an effort to branch out in a creative and lasting endeavor, I wanted to pursue a goal of becoming a Full Stack Web Developer.
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    I am currently hard at work finishing my certificate at the EdX Full Stack Web Development Bootcamp hosted by the University of Texas.
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    I am always looking for new opportunities to learn and grow as a developer, so please take a look through my portfolio and contact me if you have any questions or concerns.
-  </Typography>
-  <Typography variant="subtitle1" color="primary" sx={{ p:1}}>
-    Thank you for visiting!
-  </Typography>
-</CardContent>
+    <Box sx={{ bgcolor: 'primary.main', color: 'white', textAlign: 'center', minHeight: '100vh', p: 5 }}>
+      <Typography variant="h3" sx={{ p: 5 }}>Our Team</Typography>
 
-          </Box>
-        </Card>
+      <Grid container spacing={2}>
+        {teamMembers.map((member, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Card sx={{ background: `linear-gradient(225deg, ${theme.palette.primary.main} 0%, ${theme.palette.secondary.main} 100%)` }}>
+              <CardContent>
+                <Avatar alt={member.name} src={member.image} sx={{ width: 300, height: 300, margin: '0 auto 1rem auto' }} />
+                  <Typography variant="h5">{member.name}</Typography>
+                  <Typography variant="body1">{member.role}</Typography>
+                  <Typography variant="body1"><a href={member.github}>GitHub</a></Typography>
+                  <Typography variant="body1"><a href={member.linkedin}>LinkedIn</a></Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <Card>
-          <CardMedia
-            component="img"
-            sx={{ height: '600', width: '400' }}
-            alt="html, css, and javascript rotating in a circle"
-          />
-        </Card>
-      </Grid>
-
-    </Grid>
+    </Box>
   );
-}
+};
 
 export default About;
-
