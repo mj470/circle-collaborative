9ca2416f77dac49fec499630bb7a2a1d2cdd3544
cmdnguyen <cmdnguyen94@gmail.com>
2023-10-19 14:37:04 -0500

    Fixed the query for single group and group page can find the data

--- client/src/pages/Group.jsx
+++ client/src/pages/Group.jsx
@@ -1,14 +1,15 @@
-import { QUERY_SINGLE_GROUP } from "../utils/queries"
+import { QUERY_SINGLE_GROUP, QUERY_GROUPS } from "../utils/queries"
+import { ADD_POST } from "../utils/mutations"
 import Box from '@mui/material/Box';
 import { useParams, redirect } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { CircularProgress, Paper, Typography } from '@mui/material';
 
-const Group = ({groupId}) => {
-    // const { groupId } = useParams();
-
+export default function Group () {
+    const { groupId } = useParams();
+    const [addPost, { error }] = useMutation(ADD_POST);
     const { loading, data } = useQuery(QUERY_SINGLE_GROUP, {
-        variables: { groupId },
+        variables: {groupId}
     });
 
 
@@ -19,19 +20,14 @@ const Group = ({groupId}) => {
             </div>
         );
     }
-    const group = data?.group || {};
-    console.log(group)
+    console.log("groupdata:",data)
 
-    if (data) {
         return (
             <Paper elevation={3} style={{ padding: '20px' }}>
-                <Typography variant="h4">{group.name}</Typography>
-                <Typography variant="body1">{group.description}</Typography>
+                <Typography variant="h4">{data.group.groupName}</Typography>
+                <Typography variant="body1">{data.group.groupDescription}</Typography>
                 {/* Add Material UI components for more styling */}
             </Paper>
         )
     }
 
-}
-
-export default Group;
